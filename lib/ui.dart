76c3898471b2815368d678293457dc5399233028
ericmartineau <smartytime@gmail.com>
2021-03-16 01:02:50 -0700

    Remove flutter dep

--- lib/ui.dart
+++ lib/ui.dart
@@ -1,4 +1,3 @@
 export 'flutter_extensions/color_extensions.dart';
 export 'flutter_extensions/colors.dart';
 export 'flutter_extensions/phone_numbers.dart';
-export 'flutter_extensions/widget_extensions.dart';
